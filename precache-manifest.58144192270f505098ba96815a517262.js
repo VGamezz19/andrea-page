eb9627ec98ac00869234e325a9b9f9fb105e6a8e
VGamezz19 <victor.gamez1596@gmai.com>
2019-08-12 10:26:46 +0000

    Updates

---
+++ precache-manifest.58144192270f505098ba96815a517262.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b275a01cd7ad50b3e1533dc3947cdd64",
+    "url": "/andrea-page/index.html"
+  },
+  {
+    "revision": "d609c5210ed116a0d533",
+    "url": "/andrea-page/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7aa1b2b6c042e17ff1ed",
+    "url": "/andrea-page/static/js/2.e49df6b1.chunk.js"
+  },
+  {
+    "revision": "d609c5210ed116a0d533",
+    "url": "/andrea-page/static/js/main.61828263.chunk.js"
+  },
+  {
+    "revision": "c502a765051ec508c449",
+    "url": "/andrea-page/static/js/runtime~main.e90a933a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/andrea-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
